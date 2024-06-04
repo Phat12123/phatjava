2a78eb0f971e1e66981936360520100114c35fdd
Phat12123 <147842576+Phat12123@users.noreply.github.com>
2024-06-04 10:24:06 +0700

    Add files via upload

---
+++ phat/phat/src/main/java/com/example/phat/PhatApplication.java
@@ -0,0 +1,13 @@
+package com.example.phat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhatApplication.class, args);
+	}
+
+}
---
+++ phat/phat/src/test/java/com/example/phat/PhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
